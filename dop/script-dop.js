da9643d6d82c78fd4bf0ae7ee0dfa5d200fcd081
Nata <natalikrasnobaieva@gmail.com>
2019-09-20 17:15:15 +0300

    fixed

--- dop/script-dop.js
+++ dop/script-dop.js
@@ -13,14 +13,13 @@ function getTimeRemaining(deadline){
 }
 let newYear = getTimeRemaining('01 january 2020');
 
-
 let day,
     date = new Date(),
     greeting,
     weekDay = getWeekDay(date),
-     hour = date.getHours() - (date.getHours() >= 12 ? 12 : 0),
-     period = date.getHours() >= 12 ? 'PM' : 'AM',
-     time = hour + ':'+date.getMinutes()+':'+date.getSeconds() + period;
+    hour = date.getHours() - (date.getHours() >= 12 ? 12 : 0),
+    period = date.getHours() >= 12 ? 'PM' : 'AM',
+    time = hour + ':'+date.getMinutes()+':'+date.getSeconds() + period;
 
 function getDay(){
   let hour = new Date().getHours();
@@ -29,10 +28,9 @@ function getDay(){
         greeting = "Доброе утро";
       } else if(hour >= 12 && hour < 18){
         greeting = "Добрый день";
-      } else if(hour >= 18 && date.getHours() < 24) {
+      } else if(hour >= 18 && hour < 24) {
         greeting = "Добрый вечер";
       }
-      
       return greeting;
 }
 
@@ -41,4 +39,4 @@ const write = `<div> ${getDay()}</div>
                 <div>Текущее время:<b>${time}</b> </div>
                 <div>До нового года осталось <b> ${newYear}</b> дней</div>`
 document.write(write);
-
+console.log(window.screen)
--- script/script.js
+++ script/script.js
@@ -7,23 +7,31 @@ window.addEventListener('DOMContentLoaded', function(){
             timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
-
+        function checkTime(i) {
+                if (i < 10) {
+                  i = "0" + i;
+                }
+                return i;
+               }
         function getTimeRemaining(){
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000, 
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
+                minutesZero = checkTime(minutes),
                 hours = Math.floor(timeRemaining / 60 / 60) % 24,
+                hoursZero = checkTime(hours),
                 days = Math.floor(timeRemaining / 60 / 60 / 24);
-                return {timeRemaining, days, hours, minutes, seconds};
+                return {timeRemaining, days, hoursZero, minutesZero, seconds};
         }
         
         function updateCLock(){
            let timer = getTimeRemaining();
            timerDays.textContent = timer.days;
-           timerHours.textContent = timer.hours;
-           timerMinutes.textContent = timer.minutes;
+           timerHours.textContent = timer.hoursZero;
+          
+           timerMinutes.textContent = timer.minutesZero;
            timerSeconds.textContent = timer.seconds;
            
            if(timer.timeRemaining > 0){
@@ -33,10 +41,8 @@ window.addEventListener('DOMContentLoaded', function(){
             timerHours.textContent = '00';
             timerMinutes.textContent = '00';
             timerSeconds.textContent = '00';
-           }
-           
+           }   
         }
-
         updateCLock();
     }
 
