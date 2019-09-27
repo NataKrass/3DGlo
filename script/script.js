e2b630c414c32f4a30000e5f3afaad2a68a9e552
Nata Krass <natalikrasnobaieva@gmail.com>
2019-09-27 20:14:49 +0300

    calculator

--- script/script.js
+++ script/script.js
@@ -253,19 +253,18 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //team
     const commandPhoto = document.querySelectorAll('.command__photo');
-   
     commandPhoto.forEach((elem) => {
         let src = elem.src;
 
         elem.addEventListener('mouseenter', (event) => {
            let srcNew = event.target.dataset.img;
-           event.target.src = srcNew
-         })
+           event.target.src = srcNew;
+        })
         
          elem.addEventListener('mouseleave', (event) => {
             event.target.src = src;
             console.log(event.target.src);
-          })
+        })
     });
 
     //calculator
@@ -275,6 +274,54 @@ window.addEventListener('DOMContentLoaded', function(){
         e.value = e.value.replace(/[^\d.]/g, '');
         })
     })
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                  squareValue = +calcSquare.value;
+           
+            if(calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if(calcDay.value && calcDay < 10){
+                dayValue *= 1.5;
+                
+            }
+            console.log(dayValue)
+
+            if(typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            } 
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if(target.matches('select') || target.matches('.calc-square') ||
+            target.matches('.calc-day') || target.matches('.calc-count')) {
+               countSum();
+            }
+
+
+        })
+    }
+
+    calc(100);
+
 });
 
 //05006352577
